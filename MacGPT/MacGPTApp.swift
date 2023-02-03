f12456bfd2e3f0fe5c0d63af69f16084f3ae05b8
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2023-02-03 12:33:55 -0500

    Init project

---
+++ MacGPT/MacGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacGPTApp.swift
+//  MacGPT
+//
+//  Created by Aleksandr Molchagin on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MacGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
