e7a7b302174387af27ed362679d79342bed84c4d
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2023-02-06 11:10:14 -0500

    Revert "Add Icons"
    This reverts commit 2d8e434ec962c7ca98e81dba991173838187b1e0.

--- MacGPT/MacGPTApp.swift
+++ MacGPT/MacGPTApp.swift
@@ -15,18 +15,14 @@ struct MacGPTApp: App {
     var body: some Scene {
         WindowGroup {
             WebView(request: URLRequest(url: URL(string: "https://chat.openai.com/chat")!))
-        }
-        
-        MenuBarExtra("MacGPT", systemImage: "wand.and.stars.inverse") {
-            
-            WebView(request: URLRequest(url: URL(string: "https://chat.openai.com/chat")!))
-                .frame(width: 500, height: 700)
-                
-        }
-        .menuBarExtraStyle(.window)
-    
-        
 
+        }
+        
+        MenuBarExtra("MacGPT", systemImage: "hammer") {
+            WebView(request: URLRequest(url: URL(string: "https://chat.openai.com/chat")!))
+                
+        }.menuBarExtraStyle(.window)
+        
         
     }
 }
--- MacGPT/WebView.swift
+++ MacGPT/WebView.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 
-
 import WebKit
 import SwiftUI
 
